19b55915e54cb495374535a4083efa9a784b5565
Felipe Layos <jfelipelayos@gmail.com>
2021-03-03 13:02:25 -0500

    Pricing - quotes

--- scripts.js
+++ scripts.js
@@ -3,62 +3,137 @@ $(document).ready(() => {
         let div = document.getElementById("loader");
         let carousel = document.getElementById("carouselExampleControls");
 
-        setTimeout(() => {
-            div.classList.add("loader");
-            carousel.classList.add("d-none");
-        }, 0);
+        if (div) {
+            setTimeout(() => {
+                div.classList.add("loader");
+                carousel.classList.add("d-none");
+            }, 0);
+        }
     }
 
     function unloader() {
         let div = document.getElementById("loader");
         let carousel = document.getElementById("carouselExampleControls");
 
-        setTimeout(() => {
-            div.classList.remove("loader");
-            carousel.classList.remove("d-none");
-        }, 2000);
+        if (div) {
+            setTimeout(() => {
+                div.classList.add("loader4");
+                carousel.classList.add("d-none");
+            }, 0);
+        }
     }
 
     function loader2() {
         let div = document.getElementById("loader2");
         let carousel = document.getElementById("carousel2ExampleControls");
 
-        setTimeout(() => {
-            div.classList.add("loader2");
-            carousel.classList.add("d-none");
-        }, 0);
+        if (div) {
+            setTimeout(() => {
+                div.classList.add("loader2");
+                carousel.classList.add("d-none");
+            }, 0);
+        }
     }
 
     function unloader2() {
         let div = document.getElementById("loader2");
         let carousel = document.getElementById("carousel2ExampleControls");
 
-        setTimeout(() => {
-            div.classList.remove("loader2");
-            carousel.classList.remove("d-none");
-        }, 2000);
+        if (div) {
+            setTimeout(() => {
+                div.classList.add("loader2");
+                carousel.classList.add("d-none");
+            }, 0);
+        }
     }
 
     function loader3() {
         let div = document.getElementById("loader3");
         let carousel = document.getElementById("carousel3ExampleControls");
 
-        setTimeout(() => {
-            div.classList.add("loader");
-            carousel.classList.add("d-none");
-        }, 0);
+        if (div) {
+            setTimeout(() => {
+                div.classList.add("loader3");
+                carousel.classList.add("d-none");
+            }, 0);
+        }
     }
 
     function unloader3() {
         let div = document.getElementById("loader3");
         let carousel = document.getElementById("carousel3ExampleControls");
 
-        setTimeout(() => {
-            div.classList.remove("loader");
-            carousel.classList.remove("d-none");
-        }, 2000);
+        if (div) {
+            setTimeout(() => {
+                div.classList.add("loader3");
+                carousel.classList.add("d-none");
+            }, 0);
+        }
     }
 
+    function loader4() {
+        let div = document.getElementById("loader4");
+        let carousel = document.querySelector(".carouselExampleControls");
+
+        if (div) {
+            setTimeout(() => {
+                div.classList.add("loader4");
+                carousel.classList.add("d-none");
+            }, 0);
+        }
+    }
+
+    function unloader4() {
+        let div = document.getElementById("loader4");
+        let carousel = document.getElementById("carousel4ExampleControls");
+
+        if (div) {
+            setTimeout(() => {
+                div.classList.add("loader4");
+                carousel.classList.add("d-none"); 
+            }, 0);
+        }
+    }
+
+    function quotes2() {
+        let principalDiv = document.getElementById("carousel4Space");
+        let active = "active";
+        let inactive = "inactive";
+        loader4();
+        $.ajax({
+            url: "https://smileschool-api.hbtn.info/quotes",
+            type: "GET",
+            dataType: "json",
+            success: function (json) {
+                // console.log(json);
+                json.forEach((element) => {
+                    $("#carouselSpace").append(`
+                        <div id="carouselItem" class="carousel-item ${
+                            element.id === 1 ? active : inactive
+                        }">
+                            <div class="carousel-item-info d-flex">
+                                    <img class="d-block carousel__img mr-3" src="${
+                                        element.pic_url
+                                    }" alt="First slide">
+                                    <div class="info pb-5">
+                                        <p class="mt-4">${element.text}</p>
+                                        <p class="font-weight-bolder mb-0">${
+                                            element.name
+                                        }</p>
+                                        <p class="font-weight-light">${
+                                            element.title
+                                        }</p>
+                                    </div>
+                            </div>
+                        </div>`);
+                });
+            },
+            error: function (xhr, status) {
+                alert("Error");
+            },
+        });
+        unloader4();
+    }
     function quotes() {
         let principalDiv = document.getElementById("carouselSpace");
         let active = "active";
@@ -197,6 +272,7 @@ $(document).ready(() => {
         unloader3();
     }
     quotes();
+    quotes2();
     videos();
     videos2();
 });
