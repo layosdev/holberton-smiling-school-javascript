b0910bd634c6da713c4b725f4b658d019cb51ea6
Felipe Layos <jfelipelayos@gmail.com>
2021-03-03 12:45:38 -0500

    Homepage - latest videos

--- scripts.js
+++ scripts.js
@@ -39,6 +39,26 @@ $(document).ready(() => {
         }, 2000);
     }
 
+    function loader3() {
+        let div = document.getElementById("loader3");
+        let carousel = document.getElementById("carousel3ExampleControls");
+
+        setTimeout(() => {
+            div.classList.add("loader");
+            carousel.classList.add("d-none");
+        }, 0);
+    }
+
+    function unloader3() {
+        let div = document.getElementById("loader3");
+        let carousel = document.getElementById("carousel3ExampleControls");
+
+        setTimeout(() => {
+            div.classList.remove("loader");
+            carousel.classList.remove("d-none");
+        }, 2000);
+    }
+
     function quotes() {
         let principalDiv = document.getElementById("carouselSpace");
         let active = "active";
@@ -88,7 +108,7 @@ $(document).ready(() => {
             type: "GET",
             dataType: "json",
             success: function (json) {
-                console.log(json);
+                // console.log(json);
                 for (const element of json) {                    
                     if (element.id >= 5) {
                         break;
@@ -127,6 +147,56 @@ $(document).ready(() => {
         });
         unloader2();
     }
+    function videos2() {
+        let principalDiv = document.getElementById("videosSpace2");
+        let active = "active";
+        let inactive = "inactive";
+        loader3();
+        $.ajax({
+            url: "https://smileschool-api.hbtn.info/popular-tutorials",
+            type: "GET",
+            dataType: "json",
+            success: function (json) {
+                // console.log(json);
+                for (const element of json) {                    
+                    if (element.id >= 5) {
+                        break;
+                    }
+                    // console.log(element);
+                    $("#videosSpace2").append(`
+                        <div class="video-item video-item1 d-flex flex-column">
+                            <div class="videoThumbmail d-flex justify-content-center align-items-center">
+                                <img class="videoThumbmail-item" src="${element.thumb_url}" alt="Play">
+                                <img class="w-25 videoThumbmail-play" src="./assets/images/play.png" alt="Play">
+                            </div>
+                            <div class="videoInfo text-left">
+                                <h3 class="font-weight-bolder form-control-sm align-self-start mb-0">${element.title}</h3>
+                                <p class="ml-2 text-white-70 mt-0">${element["sub-title"]}</p>
+                                <div class="videoUserInfo d-flex mb-2 justify-content-start align-items-center">
+                                    <img class="videoUserInfoPhoto" src="${element.author_pic_url}" alt="Photo">
+                                    <h4 class="videoUserInfoName mt-2 ml-3 text-primary font-weight-bold">${element.author}</h4>
+                                </div>
+                                <div class="videoCalification d-flex justify-content-between align-items-center">
+                                    <div class="stars d-flex">
+                                        <img class="star mr-1" src="./assets/images/star_on.png" alt="star">
+                                        <img class="star mr-1" src="./assets/images/star_on.png" alt="star">
+                                        <img class="star mr-1" src="./assets/images/star_on.png" alt="star">
+                                        <img class="star mr-1" src="./assets/images/star_on.png" alt="star">
+                                        <img class="star mr-1" src="./assets/images/star_off.png" alt="star">
+                                    </div>
+                                    <p class="mt-3 text-primary font-weight-bold">${element.duration}</p>
+                                </div>
+                            </div>
+                        </div>`);
+                };
+            },
+            error: function (xhr, status) {
+                alert("Error");
+            },
+        });
+        unloader3();
+    }
     quotes();
     videos();
+    videos2();
 });
