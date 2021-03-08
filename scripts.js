b0aa697156dfe565c498ef39a4b5b1b953dd7375
Felipe Layos <jfelipelayos@gmail.com>
2021-03-08 10:44:59 -0500

    Pricing - quotes

--- scripts.js
+++ scripts.js
@@ -141,14 +141,15 @@ $(document).ready(() => {
         let principalDiv = document.getElementById("videosSpace");
         let active = "active";
         let inactive = "inactive";
-        loader2();6
+        loader2();
+        6;
         $.ajax({
             url: "https://smileschool-api.hbtn.info/popular-tutorials",
             type: "GET",
             dataType: "json",
             success: function (json) {
                 // console.log(json);
-                for (const element of json) {                    
+                for (const element of json) {
                     if (element.id >= 5) {
                         break;
                     }
@@ -178,7 +179,7 @@ $(document).ready(() => {
                                 </div>
                             </div>
                         </div>`);
-                };
+                }
             },
             error: function (xhr, status) {
                 alert("Error");
@@ -197,7 +198,7 @@ $(document).ready(() => {
             dataType: "json",
             success: function (json) {
                 // console.log(json);
-                for (const element of json) {                    
+                for (const element of json) {
                     if (element.id >= 5) {
                         break;
                     }
@@ -227,7 +228,7 @@ $(document).ready(() => {
                                 </div>
                             </div>
                         </div>`);
-                };
+                }
             },
             error: function (xhr, status) {
                 alert("Error");
@@ -235,7 +236,42 @@ $(document).ready(() => {
         });
         unloader3();
     }
+    function quotes2() {
+        let quoteSpace = document.getElementById("carouselSpace2");
+        let active = "active";
+        let inactive = "inactive";
+        if (quoteSpace) {
+            $.ajax({
+                url: "https://smileschool-api.hbtn.info/quotes",
+                type: "GET",
+                dataType: "json",
+                success: function (json) {
+                    console.log(json);
+                    json.forEach((element) => {
+                        $("#carouselSpace2").append(`
+                        <div id="carouselItem" class="carousel-item ${
+                            element.id === 1 ? active : inactive
+                        } text-white">
+                            <div class="carousel-item-info d-flex">
+                            <img class="d-block carousel__img mr-3" src="${element.pic_url}"
+                                alt="First slide">
+                            <div class="info pb-5">
+                                <p type="quotetype" class="mt-4">${element.text}</p>
+                                <p class="font-weight-bolder mb-0">${element.name}</p>
+                                <p class="font-weight-light">${element.title}</p>
+                            </div>
+                        </div>
+                        </div>`);
+                    });
+                },
+                error: function (xhr, status) {
+                    alert("Error");
+                },
+            });
+        }
+    }
     quotes();
+    quotes2();
     videos();
     videos2();
 });
